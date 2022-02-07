72f9d80828a1b48585d797512cfd8db19e4885bf
zjp <1187541244@qq.com>
2022-02-07 12:36:37 +0800

    push test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("master conflict test");
         System.out.println("hot-fix conflict test");
+        System.out.println("push test commit");
     }
 }
